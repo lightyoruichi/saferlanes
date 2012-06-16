769568f7308dd0abc6d4ad8916be33fddf5ac32e
developer <lmurray@trinistorm.org>
2012-06-15 23:51:33 -0400

    The boot script now loads the controller tables. Signed-off-by:developer <lmurray@trinistorm.org>

--- includes/saferlanes/scripts/boot.php
+++ includes/saferlanes/scripts/boot.php
@@ -1,37 +1,48 @@
 <?php
 
-// @date May 7, 2012 6:15:53 PM
-// @author Lasana Murray  <lmurray@trinistorm.org>
-// @project saferlanes
-// Copyright (C) 2012 Lasana Murray
-
-/*Class loader*/
-spl_autoload_extensions('.php');
-spl_autoload_register(function ($package)
+/**
+ *  Boot up script for saferlanes, loads the classloader and ctable.
+ *
+ */
+function classloader_setup()
 {
-     $php = '.php';
+    spl_autoload_extensions('.php');
+    spl_autoload_register(function ($package) {
+                $php = '.php';
 
-        $package = str_replace("\\", '/', $package) . $php;
+                $package = str_replace("\\", '/', $package) . $php;
 
-        if (file_exists($package))
-        {
-            require_once $package;
-            return;
-        }
-        else
-        {
-            //check the include path
-            $package = str_replace('.:', NULL, get_include_path() . DIRECTORY_SEPARATOR . $package);
-            if (file_exists($package))
-            {
-                require_once $package;
-                return;
-            }
-        }
+                if (file_exists($package))
+                {
+                    require_once $package;
+                    return;
+                }
+                else
+                {
+                    //check the include path
+                    $package = str_replace('.:', NULL, get_include_path() . DIRECTORY_SEPARATOR . $package);
+                    if (file_exists($package))
+                    {
+                        require_once $package;
+                        return;
+                    }
+                }
+            });
 
-});
+}
 
+$this->ctable = array
+        (
 
+    /*Saferlanes secret keywords*/
+    'post'=>'saferlanes\controllers\PostController',
+    'vote'=>'saferlanes\controllers\VoteController',
+    'about'=>'saferlanes\controllers\AboutPage',
+    'contact'=>'saferlanes\controllers\ContactPage',
+    'version'=>'saferlanes\controllers\Build'
+    );
+
+classloader_setup();
 
 
 ?>
